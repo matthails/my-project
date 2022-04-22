464c3b4246b0fe8c6e1dfdb9ebe03cea8a1677ae
Matt <32686182+matthails@users.noreply.github.com>
2022-04-22 21:53:37 +0100

    added react-router-dom and configured Home page route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
     <div>
-      <h1>Hello from the home page</h1>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{' '}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
-
-export default App;
