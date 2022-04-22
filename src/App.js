407922db6c279e5744e71aa9233aad76a81c3739
Matt <32686182+matthails@users.noreply.github.com>
2022-04-22 21:26:40 +0100

    added react-router-dom and configured Home page route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={Home} />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
